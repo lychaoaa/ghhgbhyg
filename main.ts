dbaae58be385ca600581abacaa420405f0eac3f6
lychaoaa <63580233+lychaoaa@users.noreply.github.com>
2025-11-15 17:48:11 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,31 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     basic.clearScreen()
 })
+input.onGesture(Gesture.Shake, function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendString("No")
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        basic.clearScreen()
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendString("Yes")
+        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        basic.clearScreen()
+    } else if (input.pinIsPressed(TouchPin.P0)) {
+        radio.sendString("Heart")
+        basic.showLeds(`
+            . . . # #
+            . . # . .
+            . # . . #
+            # . . # .
+            # . # . #
+            `)
+    } else {
+        radio.sendString("Drop away ")
+        basic.showIcon(IconNames.Heart)
+    }
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Yes") {
         basic.showIcon(IconNames.Yes)
@@ -13,6 +38,16 @@ radio.onReceivedString(function (receivedString) {
         basic.showIcon(IconNames.No)
         basic.pause(500)
         basic.clearScreen()
+    } else if (receivedString == "Heart") {
+        basic.showIcon(IconNames.Heart)
+    } else if (receivedString == "Drop away ") {
+        basic.showLeds(`
+            . . . # #
+            . . # . .
+            . # . . #
+            # . . # .
+            # . # . #
+            `)
     }
 })
 input.onButtonPressed(Button.B, function () {
